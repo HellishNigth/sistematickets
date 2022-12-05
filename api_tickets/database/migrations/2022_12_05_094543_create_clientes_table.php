55d17050739a849221bf7c6342d38df2726e7c2f
HellishNigth <kritka200@gmail.com>
2022-12-05 10:05:36 -0300

    la api

---
+++ api_tickets/database/migrations/2022_12_05_094543_create_clientes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->unsignedInteger('id')->autoIncrement();
+            $table->string('rutClie');
+            $table->string('nombreClie',50);
+            $table->string('apellidoClie',50);
+            $table->string('email',30);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
