7482e5fb346967916dc54a9efaf145134f2e1ec5
Kehmit Uribe <kehmit.uribeg@gmail.com>
2022-12-06 19:14:28 -0300

    VAMOS QUE SE PUEDE

---
+++ cliente_tickets/lib/widget/GoogleSignIn.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GoogleSignIn extends StatefulWidget {
+  GoogleSignIn({Key? key}) : super(key: key);
+
+  @override
+  State<GoogleSignIn> createState() => _GoogleSignInState();
+}
+
+class _GoogleSignInState extends State<GoogleSignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tickets For Tickets',
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
