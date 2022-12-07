b03ce538dbec382df8ec2605798e507389e1f310
HellishNigth <kritka200@gmail.com>
2022-12-07 12:55:00 -0300

    controller de tickets hecho y pivotes completados(con las rutas)

--- api_tickets/routes/api.php
+++ api_tickets/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientesController;
 use App\Http\Controllers\EventosController;
+use App\Http\Controllers\TicketsController;
 
 
 /*
@@ -23,3 +24,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('/clientes',ClientesController::class);
 Route::apiResource('/eventos',EventosController::class);
+Route::apiResource('/tickets',TicketsController::class);
