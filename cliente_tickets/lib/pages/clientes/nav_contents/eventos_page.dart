7390822fbba965d8ae08abcd261375c59f8ba191
Kehmit Uribe <kehmit.uribeg@gmail.com>
2022-12-08 09:05:41 -0300

    Listar Eventos Clientes

--- cliente_tickets/lib/pages/clientes/nav_contents/eventos_page.dart
+++ cliente_tickets/lib/pages/clientes/nav_contents/eventos_page.dart
@@ -1,12 +1,42 @@
+import 'package:cliente_tickets/providers/clientes_provider.dart';
+import 'package:cliente_tickets/providers/eventos_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class EventosClientesPage extends StatelessWidget {
   const EventosClientesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('CHUPALO POR 2'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listar Eventos'),
+      ),
+      body: FutureBuilder(
+        future: EventosProvider().getEventos(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.separated(
+            separatorBuilder: (context, index) => Divider(
+              thickness: 1.3,
+            ),
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) {
+              var evento = snapshot.data[index];
+              return ListTile(
+                leading: Icon(MdiIcons.stadium),
+                title: Text(evento['nombreEve']),
+                subtitle: Text('Ubicacion: ${evento['fechaEve']}'),
+                trailing: Text('Fecha: ${evento['fechaEve']}'),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
