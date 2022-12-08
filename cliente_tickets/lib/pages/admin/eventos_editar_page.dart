25562c9f95ddc50df31cf6a51f2d3ff7685fd571
benjassjr <benja13bebv@gmail.com>
2022-12-08 08:51:30 -0300

    Listar Eventos

---
+++ cliente_tickets/lib/pages/admin/eventos_editar_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventosEditarPage extends StatefulWidget {
+  const EventosEditarPage(evento, {key});
+
+  @override
+  State<EventosEditarPage> createState() => _EditarEventosPageState();
+}
+
+class _EditarEventosPageState extends State<EventosEditarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
