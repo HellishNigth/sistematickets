4008a5af4a3884d24c7e1cfb284b93034056f508
Kehmit Uribe <kehmit.uribeg@gmail.com>
2022-12-08 23:00:22 -0300

    Prueba Slidable

--- cliente_tickets/lib/pages/admin/listar_eventos_page.dart
+++ cliente_tickets/lib/pages/admin/listar_eventos_page.dart
@@ -4,6 +4,7 @@ import 'package:cliente_tickets/pages/admin/estado_editar_page.dart';
 import 'package:cliente_tickets/pages/admin/eventos_editar_page.dart';
 import 'package:cliente_tickets/providers/eventos_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -17,19 +18,8 @@ class ListarEventosPage extends StatefulWidget {
 class _ListarEventosPageState extends State<ListarEventosPage> {
   final fPrecio =
       NumberFormat.currency(decimalDigits: 0, locale: 'es-CL', symbol: '');
-
-  Offset _tapPosition = Offset.zero;
-  void _getTapPosition(TapDownDetails details) {
-    final RenderBox referenceBox = context.findRenderObject() as RenderBox;
-    setState(() {
-      _tapPosition = referenceBox.globalToLocal(details.globalPosition);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final RenderObject? overlay =
-        Overlay.of(context)?.context.findRenderObject();
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -53,78 +43,135 @@ class _ListarEventosPageState extends State<ListarEventosPage> {
             itemCount: snapshot.data.length,
             itemBuilder: (context, index) {
               var evento = snapshot.data[index];
-              return Dismissible(
-                  key: ObjectKey(evento),
-                  background: Container(
-                    color: Colors.purple,
-                    alignment: Alignment.centerRight,
-                    padding: EdgeInsets.only(left: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Icon(MdiIcons.noteEdit, color: Colors.white),
-                        Text(
-                          'Editar Estado',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
+              // return Dismissible(
+              //     key: ObjectKey(evento),
+              //     background: Container(
+              //       color: Colors.purple,
+              //       alignment: Alignment.centerRight,
+              //       padding: EdgeInsets.only(left: 10),
+              //       child: Row(
+              //         mainAxisAlignment: MainAxisAlignment.start,
+              //         children: [
+              //           Icon(MdiIcons.noteEdit, color: Colors.white),
+              //           Text(
+              //             'Editar Estado',
+              //             style: TextStyle(
+              //                 color: Colors.white,
+              //                 fontSize: 14,
+              //                 fontWeight: FontWeight.bold),
+              //           ),
+              //         ],
+              //       ),
+              //     ),
+              //     secondaryBackground: Container(
+              //       color: Colors.red,
+              //       alignment: Alignment.centerLeft,
+              //       padding: EdgeInsets.only(right: 10),
+              //       child: Row(
+              //         mainAxisAlignment: MainAxisAlignment.end,
+              //         children: [
+              //           Icon(MdiIcons.trashCan, color: Colors.white),
+              //           Text(
+              //             'Borrar Evento',
+              //             style: TextStyle(
+              //                 color: Colors.white,
+              //                 fontSize: 14,
+              //                 fontWeight: FontWeight.bold),
+              //           ),
+              //         ],
+              //       ),
+              //     ),
+              //     // direction: DismissDirection.endToStart,
+              //     onDismissed: (DismissDirection direction) {
+              //       if (direction == DismissDirection.endToStart) {
+              //         EventosProvider()
+              //             .borrarEvento(evento['id'].toString())
+              //             .then((fueBorrado) {
+              //           if (fueBorrado) {
+              //             snapshot.data.removeAt(index);
+              //             setState(() {});
+              //             ScaffoldMessenger.of(context).showSnackBar(
+              //               SnackBar(
+              //                 duration: Duration(seconds: 3),
+              //                 content: Text('Evento borrado'),
+              //               ),
+              //             );
+              //           } else {
+              //             ScaffoldMessenger.of(context).showSnackBar(
+              //               SnackBar(
+              //                 duration: Duration(seconds: 3),
+              //                 content: Text('No se pudo borrar'),
+              //               ),
+              //             );
+              //           }
+              //         });
+              //       } else {
+              //         MaterialPageRoute route = MaterialPageRoute(
+              //           builder: (context) =>
+              //               EstadoEditarPage(evento['id'].toString()),
+              //         );
+              //         Navigator.push(context, route).then((valor) {
+              //           setState(() {});
+              //         });
+              //       }
+              //     },
+              return Slidable(
+                  // Specify a key if the Slidable is dismissible.
+                  key: const ValueKey(0),
+
+                  // The start action pane is the one at the left or the top side.
+                  startActionPane: ActionPane(
+                    // A motion is a widget used to control how the pane animates.
+                    motion: const ScrollMotion(),
+
+                    // A pane can dismiss the Slidable.
+                    dismissible: DismissiblePane(onDismissed: () {}),
+
+                    // All actions are defined in the children parameter.
+                    children: const [
+                      // A SlidableAction can have an icon and/or a label.
+                      SlidableAction(
+                        onPressed: doNothing,
+                        backgroundColor: Color(0xFFFE4A49),
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete,
+                        label: 'Delete',
+                      ),
+                      SlidableAction(
+                        onPressed: doNothing,
+                        backgroundColor: Color(0xFF21B7CA),
+                        foregroundColor: Colors.white,
+                        icon: Icons.share,
+                        label: 'Share',
+                      ),
+                    ],
                   ),
-                  secondaryBackground: Container(
-                    color: Colors.red,
-                    alignment: Alignment.centerLeft,
-                    padding: EdgeInsets.only(right: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(MdiIcons.trashCan, color: Colors.white),
-                        Text(
-                          'Borrar Evento',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
+
+                  // The end action pane is the one at the right or the bottom side.
+                  endActionPane: const ActionPane(
+                    motion: ScrollMotion(),
+                    children: [
+                      SlidableAction(
+                        // An action can be bigger than the others.
+                        flex: 2,
+                        onPressed: doNothing,
+                        backgroundColor: Color(0xFF7BC043),
+                        foregroundColor: Colors.white,
+                        icon: Icons.archive,
+                        label: 'Archive',
+                      ),
+                      SlidableAction(
+                        onPressed: doNothing,
+                        backgroundColor: Color(0xFF0392CF),
+                        foregroundColor: Colors.white,
+                        icon: Icons.save,
+                        label: 'Save',
+                      ),
+                    ],
                   ),
-                  // direction: DismissDirection.endToStart,
-                  onDismissed: (DismissDirection direction) {
-                    if (direction == DismissDirection.endToStart) {
-                      EventosProvider()
-                          .borrarEvento(evento['id'].toString())
-                          .then((fueBorrado) {
-                        if (fueBorrado) {
-                          snapshot.data.removeAt(index);
-                          setState(() {});
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              duration: Duration(seconds: 3),
-                              content: Text('Evento borrado'),
-                            ),
-                          );
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              duration: Duration(seconds: 3),
-                              content: Text('No se pudo borrar'),
-                            ),
-                          );
-                        }
-                      });
-                    } else {
-                      MaterialPageRoute route = MaterialPageRoute(
-                        builder: (context) =>
-                            EstadoEditarPage(evento['id'].toString()),
-                      );
-                      Navigator.push(context, route).then((valor) {
-                        setState(() {});
-                      });
-                    }
-                  },
+
+                  // The child of the Slidable is what the user sees when the
+                  // component is not dragged.
                   child: ListTile(
                     leading: Text(
                       evento['nombreEve'],
@@ -140,24 +187,13 @@ class _ListarEventosPageState extends State<ListarEventosPage> {
                     ),
                     trailing: Text(evento['ubicacionEve']),
                     onLongPress: () {
-                      showMenu(
-                        items: <PopupMenuEntry>[
-                          PopupMenuItem(
-                            child: Row(
-                              children: <Widget>[
-                                Icon(Icons.delete),
-                                Text("Delete"),
-                              ],
-                            ),
-                          )
-                        ],
-                        context: context,
-                        position: RelativeRect.fromRect(
-                            Rect.fromLTWH(
-                                _tapPosition.dx, _tapPosition.dy, 30, 30),
-                            Rect.fromLTWH(0, 0, overlay!.paintBounds.size.width,
-                                overlay.paintBounds.size.height)),
+                      MaterialPageRoute route = MaterialPageRoute(
+                        builder: (context) =>
+                            EventosEditarPage(evento['id'].toString()),
                       );
+                      Navigator.push(context, route).then((valor) {
+                        setState(() {});
+                      });
                     },
                   ));
             },
@@ -167,3 +203,5 @@ class _ListarEventosPageState extends State<ListarEventosPage> {
     );
   }
 }
+
+void doNothing(BuildContext context) {}
