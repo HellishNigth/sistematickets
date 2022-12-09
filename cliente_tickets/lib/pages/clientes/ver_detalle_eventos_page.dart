613e60913d976a75a33799ca0dfb561641c48420
HellishNigth <kritka200@gmail.com>
2022-12-09 06:22:58 -0300

    ver detalle eventos

---
+++ cliente_tickets/lib/pages/clientes/ver_detalle_eventos_page.dart
@@ -0,0 +1,85 @@
+import 'package:cliente_tickets/pages/Sign_In_Screen.dart';
+import 'package:cliente_tickets/services/firestore_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  // Optional clientId
+  // clientId: '479882132969-9i9aqik3jfjd7qhci1nqf0bm2g71rm1u.apps.googleusercontent.com',
+  scopes: <String>[
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
+
+class VerDetalleEventosPage extends StatelessWidget {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  GoogleSignIn _googleSignIn = GoogleSignIn(
+    // Optional clientId
+    // clientId: '479882132969-9i9aqik3jfjd7qhci1nqf0bm2g71rm1u.apps.googleusercontent.com',
+    scopes: <String>[
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Ver detalle evento'),
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  value: 'logout',
+                  child: Text('Cerrar Sesión'),
+                ),
+              ],
+              onSelected: (opcionSeleccionada) {
+                if (opcionSeleccionada == 'logout') {
+                  _handleSignOut;
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => SignInScreen()));
+                }
+              },
+            ),
+          ],
+        ),
+        body: StreamBuilder(
+            stream: FirestroreService().noticias(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData ||
+                  snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return ListView.separated(
+                separatorBuilder: (context, index) => Divider(),
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  var detalleId = snapshot.data!.docs[index];
+                  return ListTile(
+                    leading: Icon(
+                      MdiIcons.cube,
+                      color: Colors.deepPurple,
+                    ),
+                    title: Text(detalleId['detalleEve']),
+                  );
+                },
+              );
+            }));
+  }
+
+  Future<void> _handleSignOut() => _googleSignIn.disconnect();
+
+  Future<String?> getUserEmail() async {
+    final User? user = auth.currentUser;
+    final uid = user!.email;
+
+    return uid;
+  }
+}
