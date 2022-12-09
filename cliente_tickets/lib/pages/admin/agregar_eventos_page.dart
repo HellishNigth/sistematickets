8dac325dd2763dc2b3317ce7c97f5c3f83c35a10
HellishNigth <kritka200@gmail.com>
2022-12-09 00:33:20 -0300

    gadget en agregar eventos el validator

--- cliente_tickets/lib/pages/admin/agregar_eventos_page.dart
+++ cliente_tickets/lib/pages/admin/agregar_eventos_page.dart
@@ -147,6 +147,12 @@ class _EventosAgregarPageState extends State<EventosAgregarPage> {
       decoration: InputDecoration(
         label: Text('Estado del Evento'),
       ),
+      validator: (valor){
+        if (valor == null || valor.isEmpty) {
+          return 'Indique su estado';
+        }
+        return null;
+      },
     );
   }
 
@@ -167,6 +173,12 @@ class _EventosAgregarPageState extends State<EventosAgregarPage> {
         label: Text('Precio del Evento'),
       ),
       keyboardType: TextInputType.number,
+      validator: (valor){
+        if (valor == null || valor.isEmpty) {
+          return 'Indique su Precio';
+        }
+        return null;
+      },
     );
   }
 
@@ -176,6 +188,12 @@ class _EventosAgregarPageState extends State<EventosAgregarPage> {
       decoration: InputDecoration(
         label: Text('Ubicación del Evento'),
       ),
+      validator: (valor){
+        if (valor == null || valor.isEmpty) {
+          return 'Indique su Ubicacion';
+        }
+        return null;
+      },
     );
   }
 
@@ -185,6 +203,12 @@ class _EventosAgregarPageState extends State<EventosAgregarPage> {
       decoration: InputDecoration(
         label: Text('Detalle de Evento'),
       ),
+      validator: (valor){
+        if (valor == null || valor.isEmpty) {
+          return 'Indique el Detalle';
+        }
+        return null;
+      },
     );
   }
 
@@ -194,6 +218,12 @@ class _EventosAgregarPageState extends State<EventosAgregarPage> {
       decoration: InputDecoration(
         label: Text('Nombre del Evento'),
       ),
+      validator: (valor){
+        if (valor == null || valor.isEmpty) {
+          return 'Indique Nombre';
+        }
+        return null;
+      },
     );
   }
 }
